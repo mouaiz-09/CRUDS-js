4ec92f87cca03f93ca4174faab5776aae547fc0b
moauiz-09 <rlqqyn@gmail.com>
2025-01-28 17:35:19 +0100

    add file of project mu frest project in js 2025/01/28

--- main.js
+++ main.js
@@ -7,16 +7,29 @@ let ads = document.getElementById("ads");
 let discount = document.getElementById("diss");
 let total = document.getElementById("total");
 let counet = document.getElementById("count");
-
 let submit = document.getElementById("smbt");
+let upd = document.getElementById("upd");
 total.innerHTML = " ";
 let removs = document.getElementById("clera");
 let all_remove = document.getElementById("remove");
 let cate = "";
+let mod = "creat";
+let tmp;
+
+//المنتجات انواع
+let phon = document.getElementById("phone");
+let tv = document.getElementById("tv");
+let laptop = document.getElementById("laptop");
+let tec = document.getElementById("tec");
 
 onload = function name(params) {
   showdata();
   nemberprodect();
+  sessionStorage.clear();
+  if (sessionStorage.categoury == null) {
+    console.log("framam");
+    sessionStorage.categoury = "";
+  }
 };
 
 //تحديد نوع المنتج
@@ -27,7 +40,7 @@ function cat(id) {
 }
 //عدد المنتوجات
 function nemberprodect() {
-     all_remove.innerHTML += `  "${prodect.length}"`;
+  all_remove.innerHTML = `remove all"${prodect.length}"`;
 }
 
 // حفظ ادا كان 'add new prodect'مفتوحه او مغلوق
@@ -61,9 +74,6 @@ function save(params) {
 }
 save();
 
-
-
-
 /*get totel   حساب اجمالي السعر/*/
 function totalcal(params) {
   if (pris.value != "") {
@@ -83,8 +93,6 @@ function totalcal(params) {
 }
 total.style.background = "rgb(255, 166, 0)";
 
-
-
 /* حفظ الداتا فيlocalstoreg  */
 
 let prodect;
@@ -97,23 +105,33 @@ if (localStorage.prodect != null) {
 // البيانات
 submit.onclick = function (params) {
   let newprodect = {
-    name: nameprodact.value,
+    name: nameprodact.value.toLowerCase(),
+    ads: ads.value,
+    discount: discount.value,
     prise: total.innerHTML,
     category: sessionStorage.getItem("categoury"),
     counet: counet.value,
   };
+
   /*التحقق من ان اسم المنتج غير فاضي */
 
-  if (nameprodact.value != "") {
-    //انشاء عدد من المنتوجات حسب مايدخله المستخدم
-    if (newprodect.counet > 1) {
-      for (let i = 0; i < newprodect.counet; i++) {
+  if (nameprodact.value != "" && total.innerHTML != "" && newprodect.counet <=100) {
+    if (mod === "creat") {
+      if (newprodect.counet > 1) {
+        //انشاء عدد من المنتوجات حسب مايدخله المستخدم
+        for (let i = 0; i < newprodect.counet; i++) {
+          prodect.push(newprodect);
+        }
+      } else {
         prodect.push(newprodect);
       }
     } else {
-      prodect.push(newprodect);
+      prodect[tmp] = newprodect;
+      submit.innerHTML = "creat";
+      counet.style.display = "block";
     }
 
+    // حفظ البيانات
     localStorage.setItem("prodect", JSON.stringify(prodect));
   } else {
     console.log(" دخل معلومات ياوسمك");
@@ -121,13 +139,10 @@ submit.onclick = function (params) {
 
   // تحديث البيانات  وحذف المعلونات الحالية من الانتبوت
   showdata();
-  cleredata();
+
   //عدد المنتوجات
-  nemberprodect()
- 
+  nemberprodect();
 };
-
-
 
 // clear datat:   حذف البيانات داخل input
 function cleredata() {
@@ -145,13 +160,12 @@ function showdata() {
   for (let i = 0; i < prodect.length; i++) {
     tabel += `
    <tr>
-                <td>${i+1}</td>
-                <td id="">${prodect[i].name}</td>
+                <td>${i + 1}</td>
+                <td id="">${prodect[i].name.toUpperCase()}</td>
                 <td id="df">${prodect[i].prise}</td>
-              
                 <td id="">${prodect[i].category}</td>
                 <td class="ac">
-                <button id="updet">updet</button>
+                <button onclick="updet(${i})"     id="updet">updet</button>
                 <button onclick=" remdata( ${i}) " id="delet">delet</button>
                 </td>
                 </tr>
@@ -175,8 +189,131 @@ function alldelet() {
 
 //حذف منتج معين من البيانات
 function remdata(i) {
-  console.log(i);
   prodect.splice(i, 1);
   localStorage.prodect = JSON.stringify(prodect);
   showdata();
+  nemberprodect();
+}
+function updet(i) {
+  cleredata();
+  tmp = i;
+  //جلب البيانت
+  nameprodact.value = prodect[i].name;
+  discount.value = prodect[i].discount;
+  ads.value = prodect[i].ads;
+  pris.value = prodect[i].prise;
+  total.innerHTML = prodect[i].prise;
+  totalcal();
+  counet.style.display = "none";
+
+  //تغير خلفية  لااجمالي حسب السعر
+  console.log();
+  if (prodect[i].prise > 10) {
+    total.style.background = "rgb(60, 255, 0)";
+  } else {
+    if (prodect[i].prise > 0) {
+      total.style.background = "rgb(255, 196, 0)";
+    } else {
+      total.innerHTML = "... ";
+    }
+  }
+
+  //انواع المنتجات تحديد
+  if (prodect[i].category == `"phone"`) {
+    phon.click();
+  }
+  if (prodect[i].category == `"laptop"`) {
+    laptop.click();
+  }
+  if (prodect[i].category == `"tv"`) {
+    tv.click();
+  }
+  if (prodect[i].category == `"tec"`) {
+    tec.click();
+  }
+  //تغير الزر
+  submit.innerHTML = "update";
+
+  //تغير المود
+  mod = "upd";
+  scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+//mod serch
+let sermod = "titel";
+let btn_serch = document.getElementById("serch");
+function serch(id) {
+  showdata();
+}
+//btn serch
+function cl() {
+  let bytetel = document.getElementById("bytetel");
+  let by_category = document.getElementById("by-category");
+  let div = document.getElementById("divs");
+
+  bytetel.onclick = function () {
+    by_category.classList.remove("h");
+    bytetel.classList.add("h");
+    div.classList.add("h");
+    btn_serch.classList.remove("h");
+  };
+
+  by_category.onclick = function (params) {
+    by_category.classList.add("h");
+    bytetel.classList.remove("h");
+    div.classList.remove("h");
+    btn_serch.classList.add("h");
+  };
+}
+
+//تحديد نوع المنتج
+function fat(id) {
+  sessionStorage.setItem("cat", JSON.stringify(id));
+  dz = sessionStorage.getItem("cat");
+}
+
+//serche
+function serchdata(value) {
+  let tabel = "";
+  for (let i = 0; i < prodect.length; i++) {
+    if (prodect[i].name.includes(value.toLowerCase())) {
+      tabel += `
+      <tr>
+                   <td>${i + 1}</td>
+                   <td id="">${prodect[i].name.toUpperCase()}</td>
+                   <td id="df">${prodect[i].prise}</td>
+                   <td id="">${prodect[i].category}</td>
+                   <td class="ac">
+                   <button onclick="updet(${i})"     id="updet">updet</button>
+                   <button onclick=" remdata( ${i}) " id="delet">delet</button>
+                   </td>
+                   </tr>
+                   `;
+    }
+  }
+  btn_serch.focus();
+  document.getElementById("tbody").innerHTML = tabel;
+}
+
+function srx(value) {
+  let tabel = "";
+  for (let i = 0; i < prodect.length; i++) {
+    if (prodect[i].category.includes(value.toLowerCase())) {
+      tabel += `
+    <tr>
+                 <td>${i + 1}</td>
+                 <td id="">${prodect[i].name.toUpperCase()}</td>
+                 <td id="df">${prodect[i].prise}</td>
+                 <td id="">${prodect[i].category}</td>
+                 <td class="ac">
+                 <button onclick="updet(${i})"     id="updet">updet</button>
+                 <button onclick=" remdata( ${i}) " id="delet">delet</button>
+                 </td>
+                 </tr>
+                 `;
+    }
+  }
+  document.getElementById("tbody").innerHTML = tabel;
 }
